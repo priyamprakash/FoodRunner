f9397eb891f4639034b0d47c9dbea4b7ce6b29b5
Priyam Prakash <priyamprakash99@gmail.com>
2020-08-20 05:17:19 +0530

    first commit

---
+++ app/src/main/java/com/albino/foodrunner/activity/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.albino.foodrunner.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.albino.foodrunner.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
---
+++ app/src/main/java/com/albino/foodrunner/model/RestaurantMenu.kt
@@ -0,0 +1,7 @@
+package com.albino.foodrunner.model
+
+data class RestaurantMenu (
+    var id:String,
+    var name:String,
+    var cost_for_one:String
+)
